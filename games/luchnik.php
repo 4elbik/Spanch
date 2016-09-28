cf1632f9adb5d1c859ff32757957aac61c4b5e4d
Maxim <4elbik@gmail.com>
2016-09-28 10:48:52 +0300

    Поставлен счеткчик на все остальные страницы

--- games/luchnik.php
+++ games/luchnik.php
@@ -100,7 +100,17 @@ for ($i = 0; $i < 6; $i++) {
         </div>
     </div>
     <!-- <noindex> -->
-    <div class="footer">
+    <div class="footer"><!--LiveInternet counter--><script type="text/javascript"><!--
+            document.write("<a href='//www.liveinternet.ru/click' "+
+                "target=_blank><img src='//counter.yadro.ru/hit?t15.6;r"+
+                escape(document.referrer)+((typeof(screen)=="undefined")?"":
+                ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
+                    screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
+                ";"+Math.random()+
+                "' alt='' title='LiveInternet: показано число просмотров за 24"+
+                " часа, посетителей за 24 часа и за сегодня' "+
+                "border='0' width='88' height='31'><\/a>")
+            //--></script><!--/LiveInternet-->
         <p>Copyright &copy; 2016</p>
     </div>
     <!-- </noindex> -->
